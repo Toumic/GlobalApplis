22b43a996570e08814c8686004d3e45f447a10ed
Toumic <vicente.llavata@free.fr>
2021-05-12 21:17:49 +0200

    La définition binaire qui est utilisée on peut l'éviter en inspectant les zéros. GlobModelGammy.py envoie les modèles diatoniques en chiffres_degrés, et les premiers modes en chiffres_binaires. GlobGamFonds.py est chargé de maitriser le fil binaire pour retenir uniquement les modèles fondamentaux. Ainsi que de définir le poids de chaque degré diatonique, afin d'y retenir le mode tonique

--- GlobGamFonds.py
+++ GlobGamFonds.py
@@ -1,22 +1,38 @@
 # Python 3.9 UTF-8
 # Mercredi 12 mai 2021 à 20h 32m (premières lignes)
-#
-# Conçu par Vicenté Llavata Abreu | Vicenté Quantic | Toumic
+# Cabviva.fr Cab.Rich.Int.Music.Quant
+# Conçu par Vicenté Llavata Abreu|Vicenté Quantic|Toumic
 # Module GlobGamFonds.py
-
 """Réception liste binaire Tétra + Gamme
 Priorité aux gammes heptatoniques:
     classement par le poids altéré du degré modal"""
+
 action = ['', '+', 'x', '^', '^+', '^x', '°*', '-*', '*', '°', '-']
-gamme = '1020340506078'  # Diatonisme majeur
-gammic = {'maj': '1020340506078'}
+gamme_majeure = '102034050607'  # Diatonisme naturel
+gamme_signaux = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], }
+gammic = {'maj': '102034050607'}
+gamme_pesante = {1: [[0], [0]], 2: [['b3', 'b7'], [-4, -8]],
+                 3: [['b2', 'b3', '6', 'b7'], [-3, -4, -7, -8]], 4: [['#4'], [+5]],
+                 5: [['b7'], [-8]], 6: [['b3', 'b6', 'b7'], [-4, -7, -8]],
+                 7: [['b2', 'b3', 'b5', 'b6', 'b7'], [-3, -4, -6, -7, -8]]}
+poids_major = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: []}
+poids_modal = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: []}
+# longer = {4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 12: []}
+for deg, kg in gamme_pesante.items():
+    kgk = 0
+    if len(kg[1]) == 1:
+        poids_major[deg] = kg[1][0]
+    else:
+        for k in kg[1]:
+            kgk += k
+        poids_major[deg] = kgk
+print(f' Poids maj:{poids_major}')
 
 
-def diatonic(tonic):
-    coup = 0
-    for ton in tonic:
-        if len(ton) == 13:
-
-            coup += 1
-    print(f' Coup:{coup} Action:{action}')
-    # print(f'Tonic:{tonic}')
+def diatonic(topic, top01):
+    top00 = [0]  # :top00[0]=Premier mode majeur
+    for top in topic:
+        if gamme_majeure in top:
+            top00[0] = topic.index(top)  # :top00[0]=Emplacement gamme dans topic
+            print(f" \nTopic335i:{top[-1]} Index:{top00[0]} Top01:{top01[top00[0]]}")
+            break
--- GlobModelGammy.py
+++ GlobModelGammy.py
@@ -6,8 +6,8 @@
 # Module GlobModelGammy.py
 
 """ Module d'application au traitement de la résultante clustérienne
-en une diatonie relative à la gamme naturelle musicale.
-    * L'aspect diatonique de la gamme:_ = Sur une octave de 12 notes
+en une diatonie relative à la gamme_majeure naturelle musicale.
+    * L'aspect diatonique de la gamme_majeure:_ = Sur une octave de 12 notes
     * L'aspect diatonique du tétracorde:_ = Sur l'éventail du tétra
 Il y a autant de notes que de modulations diatoniques et
 les ensembles fondamentaux n'ont pas les mêmes modulations
@@ -25,58 +25,10 @@ glob_fond = GlobGamFonds
 
 def gammy():
     cluster, coupler, recoder = [], [], []
-    diatonic_tetra = {}
-    diatonic_gamme = {}
-    zoom_local = {4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 13: []}
-    zoom_biner = []
-    binariseur = ['1111']
-
-    # Définition fondamentale de base
-    """Degrés diatonic's tétras"""
-
-    def tonale(tone):
-        # Fonction Cas(Compare/Renverse)
-        def compare(idem, tete):
-            """:idem = Liste | :tete = String"""
-            c10, t10 = tete.count('1'), ''
-            if c10 != len(idem):
-                for tee in range(len(idem)):
-                    idem.insert(0, idem.pop())
-                    eden = ''.join(i for i in idem)
-                    if eden in binariseur:
-                        break
-                else:
-                    binariseur.append(tete)
-        """ & Apprendre & Compose Long Degré
-        :Tone est un dictionnaire a clef & définition diatonique
-            Tone{1: ['1234', '2341', '3412', '4123'], 2:}
-        :Vas liste les clefs des définitions diatoniques
-            Vas[1]
-        :Clef = 1
-            Tone[Clef] = ['1234', '2341', '3412', '4123'] """
-        fille = [0]  # Longueur Tone(Values())
-        """& Apprendre & Binariser Long Degré"""
-        for uns, vas in tone.items():  # 1ère tone(Entrée tétra/gamme)
-            fille[0] = len(vas)
-            """& Apprendre Long Degré (Zoom_Local)      """
-            zoom_local[fille[0]].append(uns)
-        """& Binariser Long Degré (Zoom_Biner) = ['1111','11101','11011,,     """
-
-        for key, tilt in zoom_local.items():
-            """Exp: Zoom.() = {Key(4): Tilt([1]), K(5): T([2, 7, 22]), K(6): T([3, 8, 12, 23,]),}"""
-            for indy in tilt:  # Indy = Tilt[Value].Key()
-                titre, trans = '', []
-                entre = tone[indy][0]  # :entre = Tone[Indy][1er Degré]
-                for en in list(entre):  # :en = :entre Chiffre À Chiffre
-                    if int(en) > 0:     # Trans (1,2,3) en (1,1,1)
-                        titre += '1'    # True (0,0) en (0,0)
-                        trans.append('1')
-                    else:
-                        titre += '0'
-                        trans.append('0')
-                """Opération Compare"""
-                compare(trans, titre)
-                zoom_biner.append(titre)
+    diatonic_tetra = {4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
+    diatonic_gamme = {12: []}
+    module_gamme = []
+    bineur_gamme = []
 
     # Chargement des fichiers
     def lecteur():
@@ -102,39 +54,32 @@ def gammy():
     """RECODER GlobDicTCode = Tétras codés:1234={[#/b(1234)][#/b(5678)]} recoder[]"""
     lecteur()
 
-    # Définition diatonic_tetra tétra / gamme
+    # Définition diatonic_tetra tétra / gamme_majeure
     """Cycle degrés tétras non signés"""
 
     def transpose(module):
-        mirez, modes = '', []
-        oxo, oyo, ozo = 0, 0, module[-1]
-        while 1:
-            mirez += module[oxo]  # Donnée :module
-            if ozo != '8':
-                if module[oxo] == '4':  # Entrave :'8'
-                    oxo = 0
-                else:
-                    oxo += 1
-            else:
-                if module[oxo] == '8':  # Entrave :'8'
-                    oxo = 0
-                else:
-                    oxo += 1
-            if len(mirez) == len(module):
-                oyo += 1
-                oxo = oyo
+        mirez, modes, binez1, binez2 = '', [], '', []
+        ozo, ooo = len(module), list(module)
+        if ozo < 12:
+            for o in range(ozo):
+                ooo.insert(0, ooo.pop())
+                mirez = ''.join(i for i in ooo)
                 modes.append(mirez)
-                if ozo != '8':
-                    if mirez[0] == '4':  # Entrave :'8'
-                        break
-                else:
-                    if mirez[0] == '8':  # Entrave :'8'
-                        break
-                mirez = ''
-        if len(module) < 10:
-            diatonic_tetra[col[0]] = modes
+            diatonic_tetra[ozo].append(modes)
         else:
-            diatonic_gamme[col[0]] = modes
+            for o in range(ozo):
+                ooo.insert(0, ooo.pop())
+                mirez = ''.join(i for i in ooo)
+                modes.append(mirez)
+            for mo in modes[-1]:
+                if int(mo) > 0:
+                    binez1 += '1'
+                else:
+                    binez1 += '0'
+            binez2.append(binez1)
+            diatonic_gamme[ozo].append(modes)
+            module_gamme.append(modes)
+            bineur_gamme.append(binez2)
 
     # Origine tétra cluster[]
     """Les tétras uniques: 1234"""
@@ -143,7 +88,6 @@ def gammy():
         tr1 = str(clou[:len(clou) - 1])
         col[0] += 1
         transpose(tr1)
-    tonale(diatonic_tetra)
 
     # Fondamentales gammes coupler[]
     """Les fondamentales sont plus légères 1234,0,5678"""
@@ -151,12 +95,10 @@ def gammy():
     for clou in coupler:
         tr1 = str(clou[:len(clou) - 1])
         col[0] += 1
-        transpose(tr1)
-    tonale(diatonic_gamme)
+        transpose(tr1[:12])
 
     # Envoi Binariseur
-    glob_fond.diatonic(binariseur)
+    glob_fond.diatonic(module_gamme, bineur_gamme)
 
     # Tétracorde binaire recoder[]
     """L'alternative tétracordique 1234=#/b(1234)#/b(5678)"""
-    """Binariseur des couples tétras 99/462"""

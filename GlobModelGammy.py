1fdda290df71e8a209248630f1d8370c9011a0ee
Toumic <vicente.llavata@free.fr>
2021-06-26 17:04:30 +0200

    La définition binaire qui est utilisée on peut l'éviter en inspectant les zéros. GlobModelGammy.py envoie les modèles diatoniques en chiffres_degrés, et les premiers modes en chiffres_binaires. GlobGamFonds.py est chargé de maitriser le fil binaire pour retenir uniquement les modèles fondamentaux. Ainsi que de définir le poids de chaque degré diatonique, afin d'y retenir le mode tonique

--- GlobModelGammy.py
+++ GlobModelGammy.py
@@ -12,7 +12,7 @@ en une diatonie relative à la gamme_majeure naturelle musicale.
 Il y a autant de notes que de modulations diatoniques et
 les ensembles fondamentaux n'ont pas les mêmes modulations
 Ce module trie les diatoniques afin d'un rassemblement fondamental
-sans exécuter le traitement des tonalités avec les signatures (b/#)
+sans exécuter le traitement des tonalités avec les signes (b/#)
 Afin de faciliter le traitement, chaque entrée [1,2,3,4] devient [1,1,1,1]
     Rajouté: Pour plusieurs tétracordes de même longueur
         Une suite consécutive de zéros tient de famille diatonique
@@ -22,18 +22,19 @@ import GlobGamFonds
 
 glob_fond = GlobGamFonds
 
+fonder_tetra, fonder_gamme = [], []
+
 
 def gammy():
     cluster, coupler, recoder = [], [], []
     diatonic_tetra = {4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
     diatonic_gamme = {12: []}
-    module_gamme = []
-    bineur_gamme = []
+    module_gamme, bineur_gamme = [], []
 
     # Chargement des fichiers
     def lecteur():
-        # Chargement Fichier.txt
         """GlobDicTCord = Tétras uniques: 1234"""
+        # Chargement Fichier.txt
         fil_cluster = open('globdicTcord.txt', 'r')
         for d in fil_cluster:
             cluster.append(d)
@@ -54,11 +55,14 @@ def gammy():
     """RECODER GlobDicTCode = Tétras codés:1234={[#/b(1234)][#/b(5678)]} recoder[]"""
     lecteur()
 
-    # Définition diatonic_tetra tétra / gamme_majeure
-    """Cycle degrés tétras non signés"""
+    # Définition diatonic_tetra / Tétracordic
+    """Cycle degrés tétras non signés (Quand 12340 = 34012)"""
+    # Définition fondamental_gamme / Gammique
+    """Cumul des 66 assimilés fondamentaux binaires"""
 
     def transpose(module):
-        mirez, modes, binez1, binez2 = '', [], '', []
+        """Fonction Lecture Séquentielle Fichiers(Coupler, Cluster)"""
+        modes, binez1, binez2 = [], '', []
         ozo, ooo = len(module), list(module)
         if ozo < 12:
             for o in range(ozo):
@@ -67,19 +71,45 @@ def gammy():
                 modes.append(mirez)
             diatonic_tetra[ozo].append(modes)
         else:
-            for o in range(ozo):
+            """Flux du fichier coupler.txt Diatonic primitif"""
+            """Cycle Global"""
+            ooo.insert(0, ooo.pop())
+            while ooo[0] == '0':
                 ooo.insert(0, ooo.pop())
-                mirez = ''.join(i for i in ooo)
-                modes.append(mirez)
+            mirez = ''.join(i for i in ooo)
+            binez00 = ''
+            for mi in mirez:
+                if mi == '0':
+                    binez00 += '0'
+                else:
+                    binez00 += '1'
+            bins = list(binez00)
+            stop = False
+            for j in range(len(bins)):
+                if bins not in fonder_gamme:
+                    for i in range(len(bins)):
+                        bins.insert(0, bins.pop())
+                        while bins[0] == '0':
+                            bins.insert(0, bins.pop())
+                        break
+                else:
+                    stop = True
+            if not stop:
+                fonder_gamme.append(bins)
+
+            # Zone Fin 2 Service Interne
+            modes.append(mirez)
+
             for mo in modes[-1]:
                 if int(mo) > 0:
                     binez1 += '1'
                 else:
                     binez1 += '0'
-            binez2.append(binez1)
-            diatonic_gamme[ozo].append(modes)
-            module_gamme.append(modes)
-            bineur_gamme.append(binez2)
+            binez2.append(binez1)               # Fin 2 Service Interne
+            diatonic_gamme[ozo].append(modes)   # Fin 2 Service Interne
+            module_gamme.append(modes)          # Diatonic groupe Gammes
+            bineur_gamme.append(binez2)         # Fin 2 Service Interne
+
 
     # Origine tétra cluster[]
     """Les tétras uniques: 1234"""
@@ -89,7 +119,7 @@ def gammy():
         col[0] += 1
         transpose(tr1)
 
-    # Fondamentales gammes coupler[]
+    # Diatoniques primitives coupler[]
     """Les fondamentales sont plus légères 1234,0,5678"""
     col = [0]
     for clou in coupler:
@@ -97,8 +127,8 @@ def gammy():
         col[0] += 1
         transpose(tr1[:12])
 
-    # Envoi Binariseur
-    glob_fond.diatonic(module_gamme, bineur_gamme)
+    # Envoi 66 Modes Binarisées
+    glob_fond.diatonic(fonder_gamme)
 
     # Tétracorde binaire recoder[]
     """L'alternative tétracordique 1234=#/b(1234)#/b(5678)"""
